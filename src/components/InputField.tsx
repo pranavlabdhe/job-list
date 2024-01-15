ef8da8042ae570a90723c43b32dbb467a3869df9
pranavlabdhe <plabdhe2000@gmail.com>
2024-01-15 12:38:18 +0530

    Improved stylings

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import toDoImg from '../assets/images/to-do-list.png'
 interface Props {
   job: string;
   setJobTodo: React.Dispatch<React.SetStateAction<string>>;
@@ -29,11 +29,13 @@ const InputField: React.FC<Props> = ({ job, setJobTodo,handleJobs,hideOverlay,cl
       )}
       <div className="parent_body">
         <div className="d-flex col-12">
-            <div className="col-12 col-sm-6 col-md-6 col-lg-6 d-flex justify-content-end">
-            <h2 className="title">JOB LIST</h2>
+            <div className="col-12 col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">
+            <h2 className="title">JOB LIST <img src={toDoImg} alt="todo_image" /></h2>
             </div>
-        <div className="col-12 col-sm-6 col-md-6 col-lg-6 d-flex justify-content-end">
-            <button type="button" className="btn btn-dark" onClick={newList}>New List</button>
+        <div className="col-12 col-sm-4 col-md-4 col-lg-6 d-flex justify-content-end">
+            <button type="button" className="btn btn-light" onClick={newList}>
+                <span className="make_new_list_text"> Make New List</span>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 384 512" className="make_new_list"><path d="M280 64h40c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128C0 92.7 28.7 64 64 64h40 9.6C121 27.5 153.3 0 192 0s71 27.5 78.4 64H280zM64 112c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320c8.8 0 16-7.2 16-16V128c0-8.8-7.2-16-16-16H304v24c0 13.3-10.7 24-24 24H192 104c-13.3 0-24-10.7-24-24V112H64zm128-8a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/></svg></button>
         </div>
 
         </div>
