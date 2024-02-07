5b677f1573b7c0a1894930fa1225dfac3176705d
pranavlabdhe <plabdhe2000@gmail.com>
2024-02-07 19:53:04 +0530

    Made the styling changes

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -24,25 +24,25 @@ const InputField: React.FC<Props> = ({ job, setJobTodo,handleJobs,hideOverlay,cl
     },[newList])
   return (
     <form onSubmit={handleJobs}>
-      {hideOverlay ? null : (
+      {/* {hideOverlay ? null : (
         <div className="overlay" onClick={closeOverlayFun}></div>
-      )}
-      <div className="parent_body">
-        <div className="d-flex col-12">
-            <div className="col-12 col-sm-4 col-md-4 col-lg-4 d-flex justify-content-center">
+      )} */}
+
+        <div className="col-12 col-sm-12 col-md-12 col-lg-12 d-flex justify-content-between">
+            <div>
             <h2 className="title">JOB LIST <img src={toDoImg} alt="todo_image" /></h2>
             </div>
-        <div className="col-12 col-sm-4 col-md-4 col-lg-6 d-flex justify-content-end">
+        <div>
             <button type="button" className="btn btn-light" onClick={newList}>
                 <span className="make_new_list_text"> Make New List</span>
                 <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 384 512" className="make_new_list"><path d="M280 64h40c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128C0 92.7 28.7 64 64 64h40 9.6C121 27.5 153.3 0 192 0s71 27.5 78.4 64H280zM64 112c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320c8.8 0 16-7.2 16-16V128c0-8.8-7.2-16-16-16H304v24c0 13.3-10.7 24-24 24H192 104c-13.3 0-24-10.7-24-24V112H64zm128-8a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/></svg></button>
         </div>
-
         </div>
+       
        
         <input
           type="text"
-          className="input_field mt-3"
+          className="input_field mt-3 col-12 col-sm-12 col-md-12 col-lg-12 form-control custom-focus-color"
           onClick={showOverlayFun}
           placeholder="Enter job todo"
           value={job}
@@ -50,16 +50,16 @@ const InputField: React.FC<Props> = ({ job, setJobTodo,handleJobs,hideOverlay,cl
         />
          <input
           type="text"
-          className="input_field mt-2"
+          className="input_field mt-2 col-12 col-sm-12 col-md-12 col-lg-12 form-control custom-focus-color "
           placeholder="Search job"
           value={word}
           onChange={(e)=>setWord(e.target.value)}
         />
-        <div className="btn_wrapper">
+        <div className="btn_wrapper col-12 col-sm-12 col-md-12 col-lg-12 ">
           <button type="submit"  className="search_btn">ADD JOB</button>
         </div>
     
-      </div>
+
     </form>
   );
 };
--- src/components/JobsList.tsx
+++ src/components/JobsList.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Job } from '../model'
+import listImagUrl from '../../src/assets/images/list.png'
+import { Scrollbars } from 'react-custom-scrollbars';
 interface Props {
     jobs:any,
     setJobsTodo:any
@@ -20,12 +22,19 @@ const JobsList: React.FC<Props> = ({ jobs,setJobsTodo,word })=> {
 
   const handleToggleDone = (id:number) => {
       setJobsTodo((prevJobs:any) =>
-      prevJobs.map((job:any) =>
-      job.id === id ? { ...job, isDone: !job.isDone } : job
+      prevJobs.map((job:any) => {
+        return (
+          job.id === id ? { ...job, isDone: !job.isDone } : job 
+        )
+      }
     )
   )
   rearrangeJobs(id);
 }
+  
+  const EditText = (id:number) => {
+
+  }
 
   const handleDelete = (id:any) => {
     let getCurrentIdFromArr = jobs.filter((elem:any)=> elem.id !== id );
@@ -38,15 +47,26 @@ const JobsList: React.FC<Props> = ({ jobs,setJobsTodo,word })=> {
 
 
   return (
-    <div>
+
+        <div>
     <div className='jobs_main_div col-12'>
-      <div className='col-12 d-flex align-items-center flex-column col_6_div'>
-      <h2 className='font_class'>Jobs to do</h2>
+      <div className='col-12  d-flex align-items-center flex-column col_6_div'>
+        <div className='col-12 col-sm-8 col-md-9 col-lg-6'>
+      <Scrollbars className='job_box col-12 col-sm-6 col-md-6 col-lg-6 p-4' style={{ width:'100%' , height:'',padding:'8px' }}>
+      <div className='' style={{padding:'16px'}}>
+      { jobs.length === 0  ?
+        <div className='font_class mt-5'>
+          <img src= {listImagUrl} className='' alt='list_image' />
+          <h2>Your List will come up here</h2>
+      </div>
+      :
+      <>
         {jobs.filter((item:any)=> {
             return word.toLowerCase() === '' ? item : item.job.toLowerCase().includes(word.toLowerCase())
         }).map(({id, job, isDone}:Job)=>{
             return (
-              <div className='col-10 col-sm-6 col-md-6 col-lg-6 d-flex align-items-center gx-5 justify-space-between col_class1' key={id}>
+              <div className='d-flex mt-3'>
+              <div className='col-11 col-sm-11 col-md-11 col-lg-11 d-flex align-items-center  gx-5 justify-space-between col_class1' key={id}>
                   <div className='col-6 text-center'>
                     {isDone ? <p className='mb-2 job_text_line_through'>{job}</p> :<p className='mb-2 job_text'>{job}</p> }
                   </div>
@@ -54,25 +74,41 @@ const JobsList: React.FC<Props> = ({ jobs,setJobsTodo,word })=> {
                  <div className='actions'>
                   {
                     !isDone ? 
-                 <i className='fas fa-check-circle ic mt-1'
-                 onClick={()=>handleToggleDone(id)}
-                 ></i> : <i className='fas fa-undo ic mt-1'
+                    <>
+                      <i className='fas fa-edit edit-icon ic mt-1'
+                          onClick={()=>EditText(id)}></i> 
+                      <i className='fas fa-check-circle ic mt-1'
+                          onClick={()=>handleToggleDone(id)}></i> 
+                        
+                    </>
+               
+                 : <i className='fas fa-undo ic mt-1'
                  onClick={()=>handleToggleDone(id)}
                  ></i>
                  }
                  <i className='fas fa-trash-alt mt-1 ic' onClick={()=>handleDelete(id)}></i>             
-                  {isDone ? <p className='status_1 me-3'>Done</p> : <p className='me-3 status_2'>Not Done</p>}
+                  {isDone ? <p className='status_1 statusw me-3'>Done</p> : <p className='me-3 status_2'>Not Done</p>}
                   </div>
                   </div>
               </div>
+              </div>
             )
         })}
-        </div>
-        <div>
-
+        </>
+      }
+      </div>
+     
+      </Scrollbars>
+      </div>
         </div>
   </div>
+  <div>
+  <p className="cpy">Version: 1.0.0 </p>
+  <p className="cpy">Copyright © 2024 Pranav Labdhe</p>
   </div>
+  </div>
+ 
+  
   )
 }
 // onclick of undo show tick and change isDone to false
