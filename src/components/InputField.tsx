f6a3375ae38b81f9c12a1193953b015211f8798c
pranavlabdhe <plabdhe2000@gmail.com>
2024-02-07 20:42:21 +0530

    implemented edit functionality

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -30,12 +30,15 @@ const InputField: React.FC<Props> = ({ job, setJobTodo,handleJobs,hideOverlay,cl
 
         <div className="col-12 col-sm-12 col-md-12 col-lg-12 d-flex justify-content-between">
             <div>
-            <h2 className="title">JOB LIST <img src={toDoImg} alt="todo_image" /></h2>
+            <h2 className="title">TASK LIST <img src={toDoImg} alt="todo_image" /></h2>
             </div>
         <div>
             <button type="button" className="btn btn-light" onClick={newList}>
                 <span className="make_new_list_text"> Make New List</span>
-                <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 384 512" className="make_new_list"><path d="M280 64h40c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128C0 92.7 28.7 64 64 64h40 9.6C121 27.5 153.3 0 192 0s71 27.5 78.4 64H280zM64 112c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320c8.8 0 16-7.2 16-16V128c0-8.8-7.2-16-16-16H304v24c0 13.3-10.7 24-24 24H192 104c-13.3 0-24-10.7-24-24V112H64zm128-8a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/></svg></button>
+                <div className="make_list_img">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"  viewBox="0 0 384 512" className="make_new_list"><path d="M280 64h40c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128C0 92.7 28.7 64 64 64h40 9.6C121 27.5 153.3 0 192 0s71 27.5 78.4 64H280zM64 112c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320c8.8 0 16-7.2 16-16V128c0-8.8-7.2-16-16-16H304v24c0 13.3-10.7 24-24 24H192 104c-13.3 0-24-10.7-24-24V112H64zm128-8a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/></svg>
+                </div>
+           </button>
         </div>
         </div>
        
--- src/components/JobsList.tsx
+++ src/components/JobsList.tsx
@@ -1,115 +1,245 @@
+// import React, { useEffect, useState } from 'react'
+// import { Job } from '../model'
+// import listImagUrl from '../../src/assets/images/list.png'
+// import { Scrollbars } from 'react-custom-scrollbars';
+// interface Props {
+//     jobs:any,
+//     setJobsTodo:any
+//     wordSearch:any
+//     setWordSearch:any
+//     word:any;
+//     setWord:any
+//   }
+// const JobsList: React.FC<Props> = ({ jobs,setJobsTodo,word })=> {
+
+//   const rearrangeJobs = (id: number) => {
+//     const index = jobs.findIndex((item1: any) => item1.id === id);
+//     if (index !== -1) {
+//       const removedElement = jobs.splice(index, 1)[0];
+//       jobs.push(removedElement);
+//     }
+//   };
+
+//   const handleToggleDone = (id:number) => {
+//       setJobsTodo((prevJobs:any) =>
+//       prevJobs.map((job:any) => {
+//         return (
+//           job.id === id ? { ...job, isDone: !job.isDone } : job 
+//         )
+//       }
+//     )
+//   )
+//   rearrangeJobs(id);
+// }
+  
+//   const EditText = (id:number) => {
+
+//   }
+
+//   const handleDelete = (id:any) => {
+//     let getCurrentIdFromArr = jobs.filter((elem:any)=> elem.id !== id );
+//         setJobsTodo(getCurrentIdFromArr)
+//   }
+  
+//   useEffect(()=>{
+//     console.log(word)
+//   },[word])
+
+
+//   return (
+
+//         <div>
+//     <div className='jobs_main_div col-12'>
+//       <div className='col-12  d-flex align-items-center flex-column col_6_div'>
+//         <div className='col-12 col-sm-8 col-md-9 col-lg-6'>
+//       <Scrollbars className='job_box col-12 col-sm-6 col-md-6 col-lg-6 p-4' style={{ width:'100%' , height:'',padding:'8px' }}>
+//       <div className='' style={{padding:'16px'}}>
+//       { jobs.length === 0  ?
+//         <div className='font_class mt-5'>
+//           <img src= {listImagUrl} className='' alt='list_image' />
+//           <h2>Your List will come up here</h2>
+//       </div>
+//       :
+//       <>
+//         {jobs.filter((item:any)=> {
+//             return word.toLowerCase() === '' ? item : item.job.toLowerCase().includes(word.toLowerCase())
+//         }).map(({id, job, isDone}:Job)=>{
+//             return (
+//               <div className='d-flex mt-3'>
+//               <div className='col-11 col-sm-11 col-md-11 col-lg-11 d-flex align-items-center  gx-5 justify-space-between col_class1' key={id}>
+//                   <div className='col-6 text-center'>
+//                     {isDone ? <p className='mb-2 job_text_line_through'>{job}</p> :<p className='mb-2 job_text'>{job}</p> }
+//                   </div>
+//                   <div className='col-6 text-end '> 
+//                  <div className='actions'>
+//                   {
+//                     !isDone ? 
+//                     <>
+//                       <i className='fas fa-edit edit-icon ic mt-1'
+//                           onClick={()=>EditText(id)}></i> 
+//                       <i className='fas fa-check-circle ic mt-1'
+//                           onClick={()=>handleToggleDone(id)}></i> 
+                        
+//                     </>
+               
+//                  : <i className='fas fa-undo ic mt-1'
+//                  onClick={()=>handleToggleDone(id)}
+//                  ></i>
+//                  }
+//                  <i className='fas fa-trash-alt mt-1 ic' onClick={()=>handleDelete(id)}></i>             
+//                   {isDone ? <p className='status_1 statusw me-3'>Done</p> : <p className='me-3 status_2'>Not Done</p>}
+//                   </div>
+//                   </div>
+//               </div>
+//               </div>
+//             )
+//         })}
+//         </>
+//       }
+//       </div>
+     
+//       </Scrollbars>
+//       </div>
+//         </div>
+//   </div>
+//   <div>
+//   <p className="cpy">Version: 1.0.0 </p>
+//   <p className="cpy">Copyright © 2024 Pranav Labdhe</p>
+//   </div>
+//   </div>
+ 
+  
+//   )
+// }
+// // onclick of undo show tick and change isDone to false
+// export default JobsList
 import React, { useEffect, useState } from 'react'
 import { Job } from '../model'
 import listImagUrl from '../../src/assets/images/list.png'
 import { Scrollbars } from 'react-custom-scrollbars';
+
 interface Props {
-    jobs:any,
-    setJobsTodo:any
-    wordSearch:any
-    setWordSearch:any
-    word:any;
-    setWord:any
-  }
-const JobsList: React.FC<Props> = ({ jobs,setJobsTodo,word })=> {
+  jobs: Job[],
+  setJobsTodo: React.Dispatch<React.SetStateAction<Job[]>>,
+  wordSearch: any,
+  setWordSearch: React.Dispatch<React.SetStateAction<any>>,
+  word: any;
+  setWord: React.Dispatch<React.SetStateAction<any>>
+}
+
+const JobsList: React.FC<Props> = ({ jobs, setJobsTodo, word }) => {
+  const [editId, setEditId] = useState<number | null>(null);
+  const [editValue, setEditValue] = useState<string>('');
 
   const rearrangeJobs = (id: number) => {
-    const index = jobs.findIndex((item1: any) => item1.id === id);
+    const index = jobs.findIndex((item: Job) => item.id === id);
     if (index !== -1) {
       const removedElement = jobs.splice(index, 1)[0];
       jobs.push(removedElement);
     }
   };
 
-  const handleToggleDone = (id:number) => {
-      setJobsTodo((prevJobs:any) =>
-      prevJobs.map((job:any) => {
-        return (
-          job.id === id ? { ...job, isDone: !job.isDone } : job 
+  const handleToggleDone = (id: number) => {
+    setJobsTodo(prevJobs =>
+      prevJobs.map(job =>
+        job.id === id ? { ...job, isDone: !job.isDone } : job
+      )
+    );
+    rearrangeJobs(id);
+  };
+
+  const handleEdit = () => {
+    if (editId !== null && editValue.trim() !== '') {
+      setJobsTodo(prevJobs =>
+        prevJobs.map(job =>
+          job.id === editId ? { ...job, job: editValue } : job
         )
-      }
-    )
-  )
-  rearrangeJobs(id);
-}
-  
-  const EditText = (id:number) => {
+      );
+      setEditId(null);
+      setEditValue('');
+    }
+  };
 
-  }
+  const handleDelete = (id: number) => {
+    const updatedJobs = jobs.filter(elem => elem.id !== id);
+    setJobsTodo(updatedJobs);
+  };
 
-  const handleDelete = (id:any) => {
-    let getCurrentIdFromArr = jobs.filter((elem:any)=> elem.id !== id );
-        setJobsTodo(getCurrentIdFromArr)
-  }
-  
-  useEffect(()=>{
-    console.log(word)
-  },[word])
-
+  useEffect(() => {
+    console.log(word);
+  }, [word]);
 
   return (
-
-        <div>
-    <div className='jobs_main_div col-12'>
-      <div className='col-12  d-flex align-items-center flex-column col_6_div'>
-        <div className='col-12 col-sm-8 col-md-9 col-lg-6'>
-      <Scrollbars className='job_box col-12 col-sm-6 col-md-6 col-lg-6 p-4' style={{ width:'100%' , height:'',padding:'8px' }}>
-      <div className='' style={{padding:'16px'}}>
-      { jobs.length === 0  ?
-        <div className='font_class mt-5'>
-          <img src= {listImagUrl} className='' alt='list_image' />
-          <h2>Your List will come up here</h2>
-      </div>
-      :
-      <>
-        {jobs.filter((item:any)=> {
-            return word.toLowerCase() === '' ? item : item.job.toLowerCase().includes(word.toLowerCase())
-        }).map(({id, job, isDone}:Job)=>{
-            return (
-              <div className='d-flex mt-3'>
-              <div className='col-11 col-sm-11 col-md-11 col-lg-11 d-flex align-items-center  gx-5 justify-space-between col_class1' key={id}>
-                  <div className='col-6 text-center'>
-                    {isDone ? <p className='mb-2 job_text_line_through'>{job}</p> :<p className='mb-2 job_text'>{job}</p> }
+    <div>
+      <div className='jobs_main_div col-12'>
+        <div className='col-12 d-flex align-items-center flex-column col_6_div'>
+          <div className='col-12 col-sm-8 col-md-9 col-lg-6'>
+            <Scrollbars className='job_box col-12 col-sm-6 col-md-6 col-lg-6 p-4' style={{ width: '100%', height: '', padding: '8px' }}>
+              <div className='' style={{ padding: '16px' }}>
+                {jobs.length === 0 ?
+                  <div className='font_class mt-5'>
+                    <img src={listImagUrl} className='' alt='list_image' />
+                    <h2>Your List will come up here</h2>
                   </div>
-                  <div className='col-6 text-end '> 
-                 <div className='actions'>
-                  {
-                    !isDone ? 
-                    <>
-                      <i className='fas fa-edit edit-icon ic mt-1'
-                          onClick={()=>EditText(id)}></i> 
-                      <i className='fas fa-check-circle ic mt-1'
-                          onClick={()=>handleToggleDone(id)}></i> 
-                        
-                    </>
-               
-                 : <i className='fas fa-undo ic mt-1'
-                 onClick={()=>handleToggleDone(id)}
-                 ></i>
-                 }
-                 <i className='fas fa-trash-alt mt-1 ic' onClick={()=>handleDelete(id)}></i>             
-                  {isDone ? <p className='status_1 statusw me-3'>Done</p> : <p className='me-3 status_2'>Not Done</p>}
-                  </div>
-                  </div>
+                  :
+                  <>
+                    {jobs.filter((item: Job) => {
+                      return word.toLowerCase() === '' ? item : item.job.toLowerCase().includes(word.toLowerCase())
+                    }).map(({ id, job, isDone }: Job) => (
+                      <div className='d-flex mt-3' key={id}>
+                        <div className='col-11 col-sm-11 col-md-11 col-lg-11 d-flex align-items-center gx-5 justify-space-between col_class1'>
+                          {editId === id ? (
+                            <>
+                              <input
+                                type="text"
+                                value={editValue}
+                                className='form-control mb-3'
+                                onChange={(e) => setEditValue(e.target.value)}
+                              />
+                              <button className='btn btn-primary ms-3 mb-3' onClick={handleEdit}>Save</button>
+                              <button className='btn btn-primary ms-2 mb-3' onClick={() => setEditId(null)}>Cancel</button>
+                            </>
+                          ) : (
+                            <>
+                              <div className='col-6 text-center'>
+                                {isDone ? <p className='mb-2 job_text_line_through'>{job}</p> : <p className='mb-2 job_text'>{job}</p>}
+                              </div>
+                              <div className='col-6 text-end '>
+                                <div className='actions'>
+                                  {!isDone ? (
+                                    <>
+                                      <i className='fas fa-edit edit-icon ic mt-1'
+                                        onClick={() => setEditId(id)}></i>
+                                      <i className='fas fa-check-circle ic mt-1'
+                                        onClick={() => handleToggleDone(id)}></i>
+                                    </>
+                                  ) : (
+                                    <i className='fas fa-undo ic mt-1'
+                                      onClick={() => handleToggleDone(id)}
+                                    ></i>
+                                  )}
+                                  <i className='fas fa-trash-alt mt-1 ic' onClick={() => handleDelete(id)}></i>
+                                  {isDone ? <p className='status_1 statusw me-3'>Done</p> : <p className='me-3 status_2'>Not Done</p>}
+                                </div>
+                              </div>
+                            </>
+                          )}
+                        </div>
+                      </div>
+                    ))}
+                  </>
+                }
               </div>
-              </div>
-            )
-        })}
-        </>
-      }
-      </div>
-     
-      </Scrollbars>
-      </div>
+            </Scrollbars>
+          </div>
         </div>
-  </div>
-  <div>
-  <p className="cpy">Version: 1.0.0 </p>
-  <p className="cpy">Copyright © 2024 Pranav Labdhe</p>
-  </div>
-  </div>
- 
-  
-  )
-}
-// onclick of undo show tick and change isDone to false
-export default JobsList
+      </div>
+      <div>
+        <p className="cpy">Version: 1.0.0 </p>
+        <p className="cpy">Copyright © 2024 Pranav Labdhe</p>
+      </div>
+    </div>
+  );
+};
+
+export default JobsList;
